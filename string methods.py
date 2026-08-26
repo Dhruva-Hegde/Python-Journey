b5d62242c4255703c2745cce5ee8e35c56356964
Dhruva-Hegde <dhruvahegde55@gmail.com>
2026-08-26 12:55:01 +0530

    learn methods

---
+++ string methods.py
@@ -0,0 +1,6 @@
+message= "i am a python developer"
+
+print(message.upper()) #converts the string to uppercase
+print(message.lower()) #converts the string to lowercase
+print(message.title()) #converts the first character of each word to uppercase
+print(message.capitalize()) #converts the first character of the string to uppercase
