98b353d67371b635fff8b78e2f1baf001ed80afd
Dhruva-Hegde <dhruvahegde55@gmail.com>
2026-08-26 12:08:10 +0530

    Learn datatypes

---
+++ datatype.py
@@ -0,0 +1,17 @@
+name="Dhruva"#string
+age=23 #integer
+is_student=False #boolean
+weight=50.5 #float
+
+
+print(type(name))
+print(type(age))
+print(type(is_student))
+print(type(weight))
+
+
+s='100'
+
+addition=int(s)+age
+print(addition)
+
