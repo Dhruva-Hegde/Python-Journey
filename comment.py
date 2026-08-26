1edadd7b11ea190b37dffbc1007fef4d8159eaf2
Dhruva-Hegde <dhruvahegde55@gmail.com>
2026-08-26 12:41:41 +0530

    comments

---
+++ comment.py
@@ -0,0 +1,11 @@
+name="Dhruva"#string   #single line comment
+age=23 #integer
+is_student=False #boolean
+
+
+
+"""
+I am an MCA graduate
+I am a software engineer    #multi line comment
+I am a python developer
+""" 
