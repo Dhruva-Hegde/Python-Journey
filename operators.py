ff2b231761b0463cafe2958aba91b9bcd24424de
Dhruva-Hegde <dhruvahegde55@gmail.com>
2026-08-26 12:25:53 +0530

    learn Operators

---
+++ operators.py
@@ -0,0 +1,23 @@
+a=10
+b=20
+
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a%b)
+print(a**b)
+print(a//b)
+print(a>b)
+print(a<b)
+print(a==b)
+print(a!=b)
+print(a>=b)
+print(a<=b)
+print(a and b)
+print(a or b)
+print(not a)
+print(not b)
+print(a is b)
+
+
