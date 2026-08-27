d61eb28258b3ee98934260047cfe563cdf09df54
Dhruva-Hegde <dhruvahegde55@gmail.com>
2026-08-27 12:13:26 +0530

    learn lists

---
+++ list.py
@@ -0,0 +1,23 @@
+items=["apple","Banana","Carrot"]
+
+print(items)
+
+
+items.append("Dates") #adds an item to the end of the list
+print(items)
+
+
+items.insert(1,"Mango") #adds an item to the end of the list
+print(items)
+
+
+items.remove("Banana") #removes an item from the list
+print(items)
+
+
+print(items[0])#prints the first item in the list
+
+
+print(items[0:3:1])
+
+print(len(items))
