699b9d5cc59df94d08157d125e40dd9b6c270601
Dhruva-Hegde <dhruvahegde55@gmail.com>
2026-08-28 21:05:13 +0530

    set and its operations

---
+++ sets.py
@@ -0,0 +1,17 @@
+set={12,15,20} #unordered collection of unique elements
+
+print(set)
+
+
+print(type(set))
+
+
+
+#set operations
+
+s1={1,2,3}
+s2={3,4,5}
+
+print(s1|s2) #union
+print(s1&s2) #intersection
+print(s1-s2) #difference
