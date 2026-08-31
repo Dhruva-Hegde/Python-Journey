d3c116b22de8dd59c8bce9c2d007d645ff9918ad
Dhruva-Hegde <dhruvahegde55@gmail.com>
2026-08-31 20:16:13 +0530

    example added

---
+++ ex8.py
@@ -0,0 +1,15 @@
+time=int(input("Enter the time"))
+
+
+if time==8:
+    print("Its breakfast time")
+
+elif time==13:
+    print("Its lunch time")
+
+elif time==20:
+    print("Its dinner time")
+
+
+else:
+    print("its not a meal time")
