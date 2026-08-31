a3168aad9076b598907ef15c1b99df6137e4b2f1
Dhruva-Hegde <dhruvahegde55@gmail.com>
2026-08-31 20:11:06 +0530

    learn if,else,elif

---
+++ conditional.py
@@ -0,0 +1,13 @@
+signal=input("enter the signal color : ")
+
+if signal == "red":
+    print("STOP")
+
+elif signal == "yellow":
+    print("READY to GO")
+
+elif signal == "green":
+    print("GO")
+
+else:
+    print("Color not in the signal list")
