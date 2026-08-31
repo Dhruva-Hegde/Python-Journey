16087c387cb18c81534869035c39b137373d071a
Dhruva-Hegde <dhruvahegde55@gmail.com>
2026-08-31 20:31:44 +0530

    another example learned

---
+++ ex10.py
@@ -0,0 +1,12 @@
+att=int(input("enter your att percenmytage"))
+teacher_friend=True
+
+
+if att>=75:
+    print("eligible")
+
+elif att<75 or teacher_friend:
+    print("Elegible")
+    
+else:
+    print("Not elegible")
