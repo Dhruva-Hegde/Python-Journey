5f3bf6ab9a3ddcae858080fb110454c2a7955ea5
Dhruva-Hegde <dhruvahegde55@gmail.com>
2026-09-01 22:30:38 +0530

    even number in loops

--- loops.py
+++ loops.py
@@ -1,7 +1,13 @@
 a=True
 attempt=1
-while a and attempt<=50:
+while a :
+    if attempt%2!=0:
+        attempt=attempt+1
+        continue
+        
     print(f"try {attempt}")
     attempt=attempt+1
+    if attempt>100:
+        break
 
 print("stop")
